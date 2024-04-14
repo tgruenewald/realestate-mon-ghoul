c794e357b8efe4e4f2565dcf8869282e3926ad35
tamara188 <tamaragruenewald@gmail.com>
2024-04-14 17:41:55 -0600

    added title screen

---
+++ realestate-mon-ghoul/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    public void PlayGame(){
+        SceneManager.LoadScene ("Untitled");
+    }
+    public void ToTitleScreen(){
+        SceneManager.LoadScene ("TitleScreen");
+    }
+    public void ToWinScreen(){
+        SceneManager.LoadScene ("WinScreen");
+    }
+    
+}
